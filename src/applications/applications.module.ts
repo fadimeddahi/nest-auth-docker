3f1f24e6f68b9a5d2b5154889977f6e1c161d6fe
fadimeddahi <f_meddahi@estin.dz>
2026-01-25 21:23:55 +0000

    Fixed ApplicationsModule - added CompaniesModule import

--- src/applications/applications.module.ts
+++ src/applications/applications.module.ts
@@ -5,12 +5,14 @@ import { ApplicationsController } from './applications.controller';
 import { Application } from './application.entity';
 import { StudentsModule } from '../students/students.module';
 import { JobOffersModule } from '../job-offers/job-offers.module';
+import { CompaniesModule } from '../companies/companies.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Application]),
     forwardRef(() => StudentsModule),
     JobOffersModule,
+    CompaniesModule,
   ],
   providers: [ApplicationsService],
   controllers: [ApplicationsController],
